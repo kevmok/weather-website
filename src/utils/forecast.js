c466b32b7436b673580b1d681b99279a544ef6bc
Kevmok <mok.kevin11@gmail.com>
2021-06-22 17:46:09 -0500

    Changes to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,9 +11,10 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location!')
         } else {
             callback(undefined,
-                body.current.weather_descriptions[0]
+                'Today is a ' + body.current.weather_descriptions[0]
                 + ". It is currently " + body.current.temperature
-                + " degrees out. It feels like " + body.current.feelslike + " degrees out.")
+                + " degrees out. It feels like " + body.current.feelslike + " degrees out.\n"
+                + "Also experiencing wind speeds of " + body.current.wind_speed + ' mph.')
         }
     })
 }
