522dcb4b19f0ac3ce69a1bef5588c7ea5a58de3a
Kevmok <mok.kevin11@gmail.com>
2021-06-22 17:19:28 -0500

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -100,6 +101,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server brrrrrrrr on port :3000.')
+app.listen(port, () => {
+    console.log('Server brrrrrrrr on port ' + port)
 })
